efc29f2258e23169ea7df0107c4ea998c82b4966
Lovely <67231490+notLeeter@users.noreply.github.com>
2024-11-28 20:54:20 +0100

    Setup Wi-Fi connection and add relevant read me

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <credentials.h> // Edit Wi-Fi credentials in `include/credentials.h` for your current network
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // these are loaded from `credentials.h`
+    Serial.print("\nConnecting to Wi-Fi...");
+    while (WiFiClass::status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nSuccessfully connected to Wi-Fi!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
