550803abbbf1ea3c74f914aba0f1cd9b67cb5ddd
Lovely <67231490+notLeeter@users.noreply.github.com>
2024-12-05 13:46:43 +0100

    Setup AP point and webserver with SPIFFS

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,85 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <credentials.h> // Edit Wi-Fi credentials in `include/credentials.h` for your current network
+#include <esp_wifi.h>
+#include <SPIFFS.h>
+
+// Connection Data for ESP
+const char* AP_SSID = "GROEP_6_PROJECT_2";
+const char* AP_PASSWORD = "KGADW&^54AWDKJ&R^";
+
+WiFiServer server(80);
+String header;
+
+String state;
+
+void readMacAddress() {
+    uint8_t baseMac[6];
+    esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+    if (ret == ESP_OK) {
+        Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X\n",
+                      baseMac[0], baseMac[1], baseMac[2],
+                      baseMac[3], baseMac[4], baseMac[5]);
+    } else {
+        Serial.println("Failed to read MAC address");
+    }
+}
 
 void setup() {
     Serial.begin(9600);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // these are loaded from `credentials.h`
-    Serial.print("\nConnecting to Wi-Fi...");
-    while (WiFiClass::status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    // Serial.println("Mac Address:");
+    // readMacAddress();
+
+    if (!SPIFFS.begin()) {
+        Serial.println("An error occured while mounting SPIFFS, aborting!");
+        return;
     }
-    Serial.println("\nSuccessfully connected to Wi-Fi!");
+
+    WiFi.softAP(AP_SSID, AP_PASSWORD);
     Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.softAPIP());
+
+    server.begin();
 }
 
 void loop() {
-// write your code here
+    WiFiClient client = server.available();
+
+    if (client) {
+        Serial.println("HTTP Client Connected");
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                Serial.print(c);
+                header += c;
+
+                String path = "";
+
+                if (header.indexOf("GET /test") >= 0) {
+                    path = "/TEST";
+                    Serial.println("TEST HIT");
+                }
+                path += ".html";
+
+                if (c == '\n') {
+                    File file = SPIFFS.open(path);
+
+                    if (!file) {
+                        client.println("HTTP/1.1 404 Not Found");
+                        client.println("Content-Type: application/json");
+                        client.println("Connection: close");
+                        client.println();
+                        break;
+                    }
+
+                    client.println(file.readString());
+                    client.println();
+                    break;
+                }
+            }
+        }
+
+        header = "";
+        client.stop();
+        Serial.println("HTTP Client Disconnected\n");
+    }
 }
